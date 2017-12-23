9ca15a70e100acb367989ff7539673ee236b8851
j7666 <30362839+j7666@users.noreply.github.com>
2017-12-23 16:49:11 +0800

    设置编码为UTF-8,按照格式解析收到的socket数据，测试正常。

--- tripserver/clientsocket.cpp
+++ tripserver/clientsocket.cpp
@@ -1,6 +1,8 @@
 #include "clientsocket.h"
 #include <QDataStream>
 #include <QMessageBox>
+#include <QDateTime>
+
 
 clientsocket::clientsocket(QObject *parent): QTcpSocket(parent)
 {
@@ -11,8 +13,31 @@ clientsocket::clientsocket(QObject *parent): QTcpSocket(parent)
 void clientsocket::readData()
 {
     QDataStream in(this);
-    char buff[50];
-    memset(buff,0,sizeof(buff));
-    read(buff, 5);
-    printf("buff = %s\n",buff);
+//    char buff[50];
+//    memset(buff,0,sizeof(buff));
+//    read(buff, 5);
+//    printf("buff = %s\n",buff);
+
+    quint16 num;
+    quint8 s;
+    QString from,to;
+    QDate date;
+    QTime time;
+    in >> num;
+    in >> s;
+    in >> from;
+    in >> to;
+    in >> date;
+    in >> time;
+
+printf("num = %d\n",num);
+
+printf("s=%c\n",s);
+
+printf("from=%s\n",from.toStdString().data() );
+
+printf("to=%s\n",to.toStdString().data());
+
+printf("date=%s, time=%s\n",date.toString().toStdString().data(),time.toString().toStdString().data() );
+
 }
--- tripserver/main.cpp
+++ tripserver/main.cpp
@@ -1,10 +1,15 @@
 #include <QCoreApplication>
 #include <tripserver.h>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForTr(codec);
     tripserver ts;
     printf("===Tcp Server Runing===\n");
 
